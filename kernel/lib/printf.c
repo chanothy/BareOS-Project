4dc95dfc3eae6b280a7240de5ee4ff7dd5226df7
Timothy Chan <timchan@iu.edu>
2024-01-13 11:42:34 -0500

    %d functionality

--- kernel/lib/printf.c
+++ kernel/lib/printf.c
@@ -1,5 +1,6 @@
 #include <bareio.h>
 #include <barelib.h>
+#define buff 20
 
 /*
  *  In this file, we will write a 'printf' function used for the rest of
@@ -9,6 +10,27 @@
  *  Use "va_arg(ap, int)" to get the next argument in a variable length
  *  argument list.
  */
+
+void int_print(int num) {
+  if (num == 0) {
+    uart_putc('0');
+    return;
+  }
+
+  char buffer[buff]; 
+  int index = 0;
+
+  while (num > 0) {
+    buffer[index] = '0' + (num % 10); // convert me ascii
+    index++;
+    num /= 10;
+  }
+
+  // go back and create the num
+  for (int i = index - 1; i >= 0; i--) {
+    uart_putc(buffer[i]);
+  }
+}
 
 void printf(const char* format, ...) {
   va_list ap;
@@ -22,7 +44,8 @@ void printf(const char* format, ...) {
       letter = (char*) (format+counter);
       if (*letter == 'd') {
         // char buff[4]; // use a buffer to manage the ints
-        uart_putc(((int) va_arg(ap,int)));
+        // uart_putc(((int) va_arg(ap,int)));
+        int_print((int)va_arg(ap,int));
       }
       else if (*letter == 'x') {
         
