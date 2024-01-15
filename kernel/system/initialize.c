ef44ffc2cb59d3ffa23081b1bdd791e7c8a7a57c
Chan, Tim <timchan@iu.edu>
2024-01-15 15:08:21 -0500

    Revert "reup"
    This reverts commit 84368ba8bdd9cf28bed301cb83685e4b576b1e61.

--- kernel/system/initialize.c
+++ kernel/system/initialize.c
@@ -1,102 +1,42 @@
-#include <bareio.h>
 #include <barelib.h>
-#define buff 1024
+#include <interrupts.h>
+#include <bareio.h>
 
 /*
- *  In this file, we will write a 'printf' function used for the rest of
- *  the semester.  In is recommended  you  try to make your  function as
- *  flexible as possible to allow you to implement  new features  in the
- *  future.
- *  Use "va_arg(ap, int)" to get the next argument in a variable length
- *  argument list.
+ *  This file contains the C code entry point executed by the kernel.
+ *  It is called by the bootstrap sequence once the hardware is configured.
+ *      (see bootstrap.s)
  */
 
-void int_print(int num) {
-  if (num == 0) {
-    uart_putc('0');
-    return;
-  }
+extern uint32* text_start;    /*                                             */
+extern uint32* data_start;    /*  These variables automatically contain the  */
+extern uint32* bss_start;     /*  lowest  address of important  segments in  */
+extern uint32* mem_start;     /*  memory  created  when  the  kernel  boots  */
+extern uint32* mem_end;       /*    (see mmap.c and kernel.ld for source)    */
 
-  if (num < 0) {
-    num = num * -1;
-  }
+void ctxload(uint64**);
 
-  char buffer[buff]; 
-  int index = 0;
+uint32 boot_complete = 0;
 
-  while (num > 0) {
-    buffer[index] = '0' + (num % 10); // convert me ascii
-    index++;
-    num /= 10;
-  }
-
-  // go back and create the num
-  for (int i = index - 1; i >= 0; i--) {
-    uart_putc(buffer[i]);
-  }
-}
-
-void hex_print(int num) {
-  if (num == 0) {
-    uart_putc('0');
-    return;
-  }
-
-
-  char buffer[buff];
-  int index = 0;
-
-  if (num < 0) {
-    num = num * -1;
-  }
-
-  while (num > 0) {
-    int remainder =num % 16;
-    if (remainder < 10) {
-      // 0 ascii is 0
-      buffer[index] = ('0' + remainder);
-      index++;
-    }
-    else {
-      // a acii is 97
-      buffer[index] = 'a'+ remainder - 10;
-      index++;
-    }
-    index++;
-    num /= 16;
-  }
-
-  for (int i = index - 1; i >= 0; i--) {
-    uart_putc(buffer[i]);
-  }
+void initialize(void) {
   
-}
+  char mask;
+  mask = disable_interrupts();
+  uart_init();
+  restore_interrupts(mask);
+  boot_complete = 1;
 
-void printf(const char* format, ...) {
-  va_list ap;
-  va_start(ap, format);
+  // uint32* test = (uint32)0x23
+  printf("Kernel start: %x\n",text_start);
+  printf("--Kernel size: %d\n",(text_start-data_start));
+  printf("Globals start: %x\n",data_start);
+  printf("Heap/Stack start: %x\n",mem_start);
+  printf("--Free memory available: %d\n",(mem_start-mem_end));
+
   
-  char* letter = (char*) format;
-  int counter = 0;
-  while (*letter != '\0') {
-    if (*letter == '%') {
-      counter++;
-      letter = (char*) (format+counter);
-      if (*letter == 'd') {
-        int_print((int)va_arg(ap,int));
-      }
-      else if (*letter == 'x') {
-        uart_putc('0');
-        uart_putc('x');
-        hex_print((int)va_arg(ap,int));
-      }
-    }
-    else {
-      uart_putc(*letter);
-    }
-    counter++;
-    letter = (char*) (format+counter);
-  }
-  
-  va_end(ap);
+  // printf("\n");
+
+  // printf("hex: %x feet\n\n", 0x2ab30);
+
+  printf("hex: %x feet\n\n", 34000);
 }
