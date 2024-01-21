076f60487f4624a87b27f6bfc0eccc1bee4e81b3
Timothy Chan <timchan@iu.edu>
2024-01-21 09:34:29 -0500

    echo bugs

--- kernel/app/echo.c
+++ kernel/app/echo.c
@@ -8,10 +8,35 @@
  * line read from the UART is empty (indicated with a \n
  * followed immediately by another \n).
  */
+int getEchoText(void) {
+  char buffer[1024];
+  char c;
+  int index = 0;
+  while ((c = uart_getc()) != '\n') {
+    buffer[index] = c;
+    index++;
+  }
+  for (int i = 0; i < index; i++) {
+    uart_putc(buffer[i]);
+  }
+  if (index != 0) {
+    uart_putc('\n');
+  }
+  return index;
+}
+
 byte builtin_echo(char* arg) {
+  int loopCount;
+  int letterCount = 0;
   if (arg[4] == ' ') {
     printf(&(arg[5]));
+    printf("\n");
     return 0;
+  }
+  else {
+    while ((loopCount = getEchoText())) {
+      letterCount += loopCount;
+    }
   }
   return 0;
 }
