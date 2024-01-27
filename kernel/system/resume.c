fb3f81f5a9db327a10c5e92c7193f95ec6b8146a
Timothy Chan <timchan@iu.edu>
2024-01-27 17:45:17 -0500

    resume/suspend work

--- kernel/system/resume.c
+++ kernel/system/resume.c
@@ -1,6 +1,7 @@
 #include <barelib.h>
 #include <interrupts.h>
 #include <syscall.h>
+#include <thread.h>
 
 /*  Takes a index into the thread table of a thread to resume.  If the thread is already  *
  *  ready  or running,  returns an error.  Otherwise, adds the thread to the ready list,  *
@@ -10,6 +11,13 @@ int32 resume_thread(uint32 threadid) {
   char mask;
   mask = disable_interrupts();
 
+  if (thread_table[threadid].state == TH_SUSPEND) {
+    thread_table[threadid].state = TH_READY;
+    raise_syscall(RESCHED);
+  }
+  else {
+    threadid = -1;
+  }
 
   restore_interrupts(mask);
   return threadid;
--- kernel/system/suspend.c
+++ kernel/system/suspend.c
@@ -12,14 +12,13 @@ int32 suspend_thread(uint32 threadid) {
   char mask;
   mask = disable_interrupts();
 
-  // thread_t currThr = thread_table[threadid]; // grabs thread from table
-  // if (currThr != TH_RUNNING || currThr != TH_READY) {
-  //   printf("error");
-  //   return 1;
-  // }
-  // else {
-  //   currThr = TH_SUSPEND;
-  // }
+  if (thread_table[threadid].state != TH_SUSPEND) {
+    thread_table[threadid].state = TH_SUSPEND;
+    raise_syscall(RESCHED);
+  }
+  else {
+    threadid = -1;
+  }
   
   restore_interrupts(mask);
   return 0;
